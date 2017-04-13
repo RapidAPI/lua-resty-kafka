98ebfbc992edfe013ab05a974585f63d4f12ae81
Andrey Bukati <andrey@bukati.me>
2017-04-13 20:10:29 +0300

    added support to Kafka 0.10

--- lib/resty/kafka/response.lua
+++ lib/resty/kafka/response.lua
@@ -2,6 +2,7 @@
 
 
 local bit = require "bit"
+local request = require "resty.kafka.request"
 
 
 local setmetatable = setmetatable
@@ -15,12 +16,14 @@ local strbyte = string.byte
 local _M = {}
 local mt = { __index = _M }
 
+function _M.new(self, str, api_version)
+    api_version = api_version or request.API_VERSION_V0
 
-function _M.new(self, str)
     local resp = setmetatable({
         str = str,
         offset = 1,
         correlation_id = 0,
+        api_version = api_version,
     }, mt)
 
     resp.correlation_id = resp:int32()
@@ -37,8 +40,8 @@ function _M.int16(self)
     local high = byte(str, offset)
     -- high padded
     return bor((high >= 128) and 0xffff0000 or 0,
-            lshift(high, 8),
-            byte(str, offset + 1))
+        lshift(high, 8),
+        byte(str, offset + 1))
 end
 
 
